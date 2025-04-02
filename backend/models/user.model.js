3e7329611d9a6e4e4bfe39dd0a3e1c37259402e8
bence <67009276+bqnce@users.noreply.github.com>
2025-04-03 00:49:43 +0200

    Updated User Model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,8 +1,21 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true }
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true, enum: ["admin", "customer", "user"] },
+  isVerified: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now },
+  lastLogin: { type: Date },
+  status: {
+    type: String,
+    enum: ["active", "banned", "inactive"],
+    default: "active",
+  },
+  profilePicture: { type: String },
+  passwordResetToken: { type: String },
+  passwordResetExpires: { type: Date },
 });
 
-export const Users = mongoose.model('user', userSchema, 'users');
+export const Users = mongoose.model("user", userSchema, "users");

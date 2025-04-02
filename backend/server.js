cd0514829e6b93501875a6f5d65dd3a4ec94a571
bence <67009276+bqnce@users.noreply.github.com>
2025-04-03 00:39:25 +0200

    Bcrypt, JWT install. Basic auth endpoints, models, middlewares, controllers

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv';
 import { connect } from './database/connect.js';
+import authRoutes from './routes/auth.routes.js';
 dotenv.config();
 
 const app = express();
@@ -10,9 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json('Backend működik!')
-});
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
     console.log(`A szerver fut a ${PORT} porton`)
